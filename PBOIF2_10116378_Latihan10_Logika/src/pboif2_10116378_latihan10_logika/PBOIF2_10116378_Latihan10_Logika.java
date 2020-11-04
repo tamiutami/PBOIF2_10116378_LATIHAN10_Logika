adc4705acef4605f8761260309c6d4321a18b466
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-05 03:06:02 +0700

    Add files via upload

---
+++ PBOIF2_10116378_Latihan10_Logika/src/pboif2_10116378_latihan10_logika/PBOIF2_10116378_Latihan10_Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116378_latihan10_logika;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-IF2
+ * Nim   : 10116378
+ */
+public class PBOIF2_10116378_Latihan10_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
